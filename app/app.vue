882ff2390c50f4b41614d08698ff0010ba86556f
Missael <lmpadillar@gmail.com>
2026-06-16 19:55:16 -0700

    Add sleep utility and update app.vue to use it for delayed count initialization

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+
+const count = ref(0);
+await sleep(2000);
+
+
+count.value = 20;
+</script>
+
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Hola mundo - {{ count }}</h1>
+    <button @click="count++">+</button>
   </div>
 </template>
