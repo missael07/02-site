b6f93c538f5f3c3389aa053c1137355b25fc6008
Missael <lmpadillar@gmail.com>
2026-06-20 13:11:28 -0700

    fix(auth): improve logging in not-authenticated middleware for better debugging

--- app/middleware/not-authenticated.ts
+++ app/middleware/not-authenticated.ts
@@ -1,9 +1,9 @@
 export default defineNuxtRouteMiddleware( async (to, from) => {
   const { isAuthenticated, fetch } = useAuthentication();
   const notAuthenticatedRoutes = ["/login", "/register"];
-
+    console.log('guest middleware:', isAuthenticated.value)
     await fetch()
-
+    console.log('guest middleware:', isAuthenticated.value)
   if (notAuthenticatedRoutes.includes(to.path) && isAuthenticated.value) {
     return navigateTo("/");
   }
